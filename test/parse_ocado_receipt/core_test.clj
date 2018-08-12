fd8d69e2639e8cc534a9b219234726ab0b8273fb
Benedek Fazekas <benedek.fazekas@gmail.com>
2018-08-12 18:03:30 +0100

    initial commit

---
+++ test/parse_ocado_receipt/core_test.clj
@@ -0,0 +1,7 @@
+(ns parse-ocado-receipt.core-test
+  (:require [clojure.test :refer :all]
+            [parse-ocado-receipt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
